a99c3e9128c60ed948c9fd7b4e214a3ab2b04c82
copper-blue <copper-blue@users.noreply.github.com>
2017-06-13 10:39:36 -0400

    Added nunjucks package and templating code

--- app.js
+++ app.js
@@ -2,13 +2,29 @@
 // No path necessary b/c module is built-in
 const express = require('express');
 const app = express();
-var morgan = require('morgan')
+const morgan = require('morgan')
+const nunjucks = require('nunjucks');
 
 // morgan
 app.use(morgan('combined'));
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
+
 app.get('/', (req, res) => {
-    res.send('My Twitter app is connected!')
+    //res.send('My Twitter app is connected!');
+    let context = {
+        title: 'An Example',
+        people: [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]
+    };
+
+    // nunjucks.render("index.html", context, (err, output) => {
+    //     res.send(output);
+    // });
+
+    res.render("index", context);
 })
 
 app.get('/news', (req, res) => {
